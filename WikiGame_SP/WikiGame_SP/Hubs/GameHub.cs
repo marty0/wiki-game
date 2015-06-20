c5414523b780d1266e7e18bcd65e9145bc7d2436
gyosifov <george.josifov@gmail.com>
2015-06-20 19:49:56 +0300

    Changes of the GameHub

--- WikiGame_SP/WikiGame_SP/Hubs/GameHub.cs
+++ WikiGame_SP/WikiGame_SP/Hubs/GameHub.cs
@@ -1,9 +1,47 @@
 ﻿namespace WikiGame_SP.Hubs
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Microsoft.AspNet.SignalR;
+    using Models.Multiplayer;
 
     public class GameHub : Hub
     {
+        private static Dictionary<string, List<Player>> GameRooms = new Dictionary<string, List<Player>>();
+        private static Dictionary<string, Game> Games = new Dictionary<string, Game>();
 
+        public void StartGame(string category, string username)
+        {
+            var newPlayer = new Player
+            {
+                Name = username,
+                ConnectionId = Context.ConnectionId
+            };
+
+            if (GameRooms.ContainsKey(category))
+            {
+                if (GameRooms[category].Any())
+                {
+                    var playerOne = GameRooms[category].First();
+
+                    Games.Add(string.Format("{0}_{1}", playerOne.ConnectionId, newPlayer.ConnectionId), new Game
+                    {
+                        PlayerOne = playerOne,
+                        PlayerTwo = newPlayer,
+                        StartGame = DateTime.Now,
+                        CategoryName = category
+                    });
+                }
+                else
+                {
+                    GameRooms[category].Add(newPlayer);
+                }
+            }
+            else
+            {
+                GameRooms.Add(category, new List<Player> { newPlayer });
+            }
+        }
     }
 }
--- WikiGame_SP/WikiGame_SP/Utilities/UserInformation.cs
+++ WikiGame_SP/WikiGame_SP/Utilities/UserInformation.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace WikiGame.Utilities
+﻿namespace WikiGame.Utilities
 {
     public class UserInformation
     {
-
         public string CategoryName { get; set; }
-
     }
 }
