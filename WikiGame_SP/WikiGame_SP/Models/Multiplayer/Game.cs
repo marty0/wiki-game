c01d38591460919299ad37d8cd03ca8ab844cc69
gyosifov <george.josifov@gmail.com>
2015-06-20 19:50:12 +0300

    Added models for games and players

---
+++ WikiGame_SP/WikiGame_SP/Models/Multiplayer/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace WikiGame_SP.Models.Multiplayer
+{
+    using System;
+
+    public class Game
+    {
+        public DateTime StartGame { get; set; }
+
+        public DateTime EndGame { get; set; }
+
+        public Player PlayerOne { get; set; }
+
+        public Player PlayerTwo { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
---
+++ WikiGame_SP/WikiGame_SP/Models/Multiplayer/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace WikiGame_SP.Models.Multiplayer
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        
+        public string ConnectionId { get; set; }
+    }
+}
