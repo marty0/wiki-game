c82b4e2dd3d9e5cc0a5660010ea8a4b9ead5dd5d
dkamburov <DKamburov@IGBGSOFASP44.infragistics.local>
2015-07-07 13:41:36 +0300

    Add more categories

--- WikiGame_SP/WikiGame_SP/Models/TestCategoryProvider.cs
+++ WikiGame_SP/WikiGame_SP/Models/TestCategoryProvider.cs
@@ -148,9 +148,38 @@ namespace WikiGame.Models
             cat1.Name = "Game Mode Easy";
             cat1.KeyWords.AddRange(new string[] { "a", "an", "of", "the", "and","for","on" });
 
+            GameCategory cat2 = new GameCategory();
+            cat2.Name = "Balkans";
+            cat2.KeyWords.AddRange(new string[] { "albania", "bosnia and herzegovina", "bulgaria", "croatia", "greece", "kosovo", "macedonia", "montenegro", "romania", "slovenia", "serbia", "turkey" });
+
+            GameCategory cat3 = new GameCategory();
+            cat3.Name = "Bulgaria";
+            cat3.KeyWords.AddRange(new string[] { "bulgaria" });
+
+            GameCategory cat4 = new GameCategory();
+            cat4.Name = "Eight-thousanders";
+            cat4.KeyWords.AddRange(new string[] { "mount everest", "k2", "kangchenjunga ", "lhotse", "makalu", "cho oyu", "dhaulagiri", "manaslu", "nanga parbat", "annapurna", "gasherbrum i", "broad peak ", "gasherbrum ii", "shishapangma"});
+
+            GameCategory cat5 = new GameCategory();
+            cat5.Name = "Mammals";
+            cat5.KeyWords.AddRange(new string[] { "alpaca", "cow", "donkey", "cat", "dog", "fox", "sheep", "horse", "coat", "elephant", "rodent", "ferret", "liama", "pig", "rabbit", "rat", "mice", "hamster", "pig", "gerbil", "chinchila" });
+
+            GameCategory cat6 = new GameCategory();
+            cat6.Name = "Top 10 Best Love Songs Of All Time";
+            cat6.KeyWords.AddRange(new string[] { "higher love", "iris", "you needed me", "baby baby", "rosanna", "escape", "danny's song", "what's my name?", "lovesong", "love theme from a star is born" });
+
+            GameCategory cat7 = new GameCategory();
+            cat7.Name = "Provinces of Bulgaria";
+            cat7.KeyWords.AddRange(new string[] { "blagoevgrad", "burgas", "dobrich", "gabrovo", "haskovo", "kardzhali", "kyustendil", "lovech", "montana", "pazardzhik", "pernik", "pleven", "plovdiv", "razgrad", "ruse", "shumen", "silistra", "sliven", "smolyan", "sofia city", "sofia", "stara zagora", "targovishte", "varna", "veliko tarnovo", "vidin", "vratsa", "yambol" });
 
             this.AddCategory(cat0);
             this.AddCategory(cat1);
+            this.AddCategory(cat2);
+            this.AddCategory(cat3);
+            this.AddCategory(cat4);
+            this.AddCategory(cat5);
+            this.AddCategory(cat6);
+            this.AddCategory(cat7);
         }
 
 
